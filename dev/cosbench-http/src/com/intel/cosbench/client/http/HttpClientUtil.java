fc6f006cec70703e8e1ae8a9b8c29f02e4521aa5
sineio <sinecelia.wang@gmail.com>
2024-08-05 16:59:43 +0800

    remove newer httpclient and httpcore

--- dev/cosbench-http/src/com/intel/cosbench/client/http/HttpClientUtil.java
+++ dev/cosbench-http/src/com/intel/cosbench/client/http/HttpClientUtil.java
@@ -17,7 +17,6 @@ limitations under the License.
 
 package com.intel.cosbench.client.http;
 
-import java.io.IOException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
@@ -26,10 +25,6 @@ import javax.net.ssl.X509TrustManager;
 
 import org.apache.commons.codec.EncoderException;
 import org.apache.commons.codec.net.URLCodec;
-import org.apache.http.HttpException;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpResponseInterceptor;
-import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
@@ -42,14 +37,11 @@ import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.SingleClientConnManager;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.params.HttpProtocolParams;
-import org.apache.http.protocol.HttpContext;
-import org.omg.CORBA.PUBLIC_MEMBER;
 
 /**
  * This class encapsulates basic HTTP client related functions which are
